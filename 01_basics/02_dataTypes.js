0c0987ad5d3d12f435fd80ab070a54683ce7f6da
Roshan Sah <118677669+roshansah21@users.noreply.github.com>
2024-06-04 14:09:00 +0000

    DataTypes done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,34 @@
+ "use strict" ; // treat as JS code newer version
+
+//   alert (2 + 2) we are using node js, not browser
+
+console.log(2 
+    + 2); // code readability should be high
+
+    // USE DOCUMENTATION MDN and TC39 
+
+console.log("Roshan");
+
+
+let name = "Roshan"
+let age = 23
+let isLoggedIn = false
+let state ;//empty null
+
+
+
+// number => 2 to power 53
+// bigint => trading, stockMarket
+// string => " "
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => use in react to find UNIQUEness
+
+
+// OBJECT in details
+
+console.log(typeof age)
+console.log(typeof null) // output null is object
+
+
